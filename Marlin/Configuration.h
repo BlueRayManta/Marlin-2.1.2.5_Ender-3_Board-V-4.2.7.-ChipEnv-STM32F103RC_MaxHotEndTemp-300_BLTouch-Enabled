0a9b76849a75e1100bd27e7aa9b04691d58756c2
BlueRayManta <148062050+BlueRayManta@users.noreply.github.com>
2026-01-08 22:55:53 +0100

    enabled host commands

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1514,7 +1514,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -4, -2.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
